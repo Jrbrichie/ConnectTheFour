1a1917c38439cf8f3cdb9da4ef1bffa4f9eb63e5
jrbrichie <jrbrichie@gmail.com>
2023-05-27 16:01:29 +0700

    home 27/05/23 16:00
    - player cannot place a token in a full column
    - player wins if they get a vertical or horizontal win
    - the winning player gets the appropriate win message
    - fixed and standardized code comments

--- board.py
+++ board.py
@@ -4,7 +4,7 @@ class Board:
     """
     print_board() prints the board in the proper format
     since the lists within the board are actually columns in the game,
-    the printing of rows and columns are reversed
+    the printing of rows and columns are reversed to suit the Connect the Four token-placing mechanic
     """
     def print_board(self):
         print("+---+---+---+---+---+---+---+")
@@ -20,26 +20,27 @@ class Board:
 
 
     """
-    valid_column(col) takes in a column argument that the user inputs.
+    valid_column(col) takes in a column argument where the player wants to place their token
     a column is valid if the column is not full, the column does not exceed 6 or is not less than 1
     and if the column is an integer.
     :returns true if position is valid 
     """
     def valid_column(self, col):
         valid_col = False
-        columns = ["1", "2", "3", "4", "5", "6", "7"]
+        column_nums = ["1", "2", "3", "4", "5", "6", "7"]
 
-        if col in columns:
+        # checks the string version of col is a string version of a column
+        if col in column_nums:
             col = int(col)
-            valid_col = 7 > col >= 0 and not self.is_col_full(col)
+            valid_col = not self.board[col - 1][5] != " "  # is true when the column is not full
 
         return valid_col
 
 
     """
-	place_token takes in a token variable and the column where the player wants to place their token
+	place_token(token, col) takes in the player's token and the column where the player wants to place their token
 	it pushes a token into the next available column given that the column is not empty
-	returns true if token placement is successful, else it returns false
+	:returns true if token placement is successful, else it returns false
 	"""
     def place_token(self, token, col):
         for i in range(len(self.board[col])):
@@ -47,16 +48,10 @@ class Board:
                 self.board[col][i] = token
                 break
 
-    """
-    is_col_full takes in a col argument that the user inputs
-    :returns true if the column still has space, false if it is full 
-    """
-    def is_col_full(self, col):
-        return self.board[col][5] != " "
 
     """
-	is_board_full checks if all positions in the board have been filled
-	if full it returns true
+	is_board_full() checks if all positions in the board have been filled
+	:returns true if board is full
 	"""
     def is_board_full(self):
         is_full = True
@@ -73,40 +68,75 @@ class Board:
         return is_full
 
     """
-	vertical_win checks if that player has 4 tokens vertically, 
-	it returns the token if they have four tokens on top of each other 
+	vertical_win(token) takes in a token argument that belonging to the player
+	it checks if that player has 4 tokens vertically, 
+	:returns true if there are four tokens on top of each other 
 	"""
+    def vertical_win(self, token):
+        for i in range(0, 7):
+            for j in range(0, 3):
+                win = True
 
-    def vertical_win(self):
-        pass
+                # goes through each adjacent vertical position next to current to see if it is the same as the token
+                for k in range(0, 4):
+                    if token != self.board[i][j + k]:
+                        win = False
+                        break
+
+                # if all 4 are the same, then the method returns true
+                if win:
+                    return True
+
+                #
+                # # this checks the first and fourth adjacent vertical tokens needed for a vertical win
+                # # it removes the need for a loop which is less efficient considering it needs to loop through all 4 tokens
+                # if b[i][j] == b[i][j + 3]:
+                #     if b[i][j] == b[i][j + 1] and b[i][j] == b[i][j + 2]:
+                #         return True
+
+        return False
+
 
     """
-	horizontal_win checks if that player has 4 tokens horizontally, 
-	it returns the token if they have four tokens next to each other 
+	horizontal_win(token) takes in a token argument that belonging to the player
+	it checks if that player has 4 tokens horizontally, 
+	:returns true if there are four tokens next to each other 
 	"""
+    def horizontal_win(self, token):
+        # goes through first 4 columns and all rows of the visible board (NOT THE ACTUAL 2D ARRAY BOARD)
+        for i in range(0, 4):
+            for j in range(0, 6):
+                win = True
 
-    def horizontal_win(self):
-        pass
+                # goes through each adjacent horizontal position next to current to see if it is the same as the token
+                for k in range(0, 4):
+                    if token != self.board[i + k][j]:
+                        win = False
+                        break
+
+                # if all 4 are the same, then the method returns true
+                if win:
+                    return True
+
+        return False
 
     """
-	diagonal_right_win checks if that player has 4 tokens vertically, 
+	diagonal_right_win() checks if that player has 4 tokens vertically, 
 	it returns the token if they have four tokens diagonally to the right
 	"""
-
     def diagonal_right_win(self):
         pass
 
     """
-	diagonal_left_win checks if that player has 4 tokens vertically, 
+	diagonal_left_win() checks if that player has 4 tokens vertically, 
 	it returns the token if they have four tokens diagonally to the left 
 	"""
-
     def diagonal_left_win(self):
         pass
 
     """
 	player_win takes in a player token variable and checks all win conditions
+	:returns true if any of the win conditions are met 
 	"""
-
-    def player_win(self, p):
-        pass
+    def player_win(self, token):
+        return self.vertical_win(token) or self.horizontal_win(token)
--- game.py
+++ game.py
@@ -7,12 +7,9 @@ class Game:
     p2 = "O"
 
     """
-	the recursive function turn() takes in the option for who goes first
-	if it is p1, then it sets the p1_turn variable to true
-	else if the option is not p2, then it calls the turn() function again with a new input 
-	returns if player 1 goes first
+	turn(option) takes in the option for who goes first
+	it uses recursion that keeps asking the player to choose who goes first until either p1/p2 is inputted
 	"""
-
     def turn(self, option):
 
         p1_turn = False
@@ -20,37 +17,46 @@ class Game:
         if option == "p1":
             p1_turn = True
         elif option != "p2" and option != "p1":
-            p1_turn = self.turn(input(option + " is not an option! Please enter a valid option: "))
+
+            # calls the turn(option) function again to check if the new input is valid
+            new_p1_turn = self.turn(input(option + " is not an option! Please enter a valid option: "))
+            p1_turn = new_p1_turn
 
         return p1_turn
 
     """
-	win_message outputs the appropriate win message depending on who wins
-	"""
+    win_message(p1_turn) takes in the p1_turn determining whose turn it was when the game was won
+    it prints the win message with the appropriate player
+    """
+    def win_message(self, p1_turn):
+        player = "2"
+        if p1_turn:
+            player = "1"
 
-    def win_message(self, p):
-        pass
+        print("")
+        self.b.print_board()
+        print("\nPlayer " + player + " has won Connect the Four! Congratulations!!!")
 
     """
-	game_loop takes in the token characters of both players
-	if continues running until a player wins OR the board is full
+	game_loop(p1_turn) takes p1_turn to determine which player goes first
+	continues running until a player wins OR the board is full
 	"""
-
-    def game_loop(self, p1_first):
+    def game_loop(self, p1_turn):
+        player_won = False
 
         # continues running as long as the board is not full
         while not self.b.is_board_full():
             self.b.print_board()
 
             # determines the message and token for the player currently playing
-            if p1_first:
+            if p1_turn:
                 p_text = "Player 1"
                 p_token = self.p1
             else:
                 p_text = "Player 2"
                 p_token = self.p2
 
-            if p1_first:
+            if p1_turn:
                 print("Player 1's turn!")
             else:
                 print("Player 2's turn!")
@@ -69,16 +75,26 @@ class Game:
             self.b.place_token(p_token, column)  # places the current player's token in their desired column
 
             # TODO create and implement win conditions
-            # self.b.player_win(p_token)
+            if self.b.player_win(p_token):
+                player_won = True
+                break
 
-            p1_first = not p1_first
+            p1_turn = not p1_turn  # gives the other player their turn
+
+        # if a player wins, a win message is outputted,
+        if player_won:
+            self.win_message(p1_turn)
+        # otherwise it outputs that the board is full and asks if they want to play again
+        else:
+            # TODO create board full and play again mechanics
+            print("Board is full! Play again? (y/n): ")
 
     """
     play() asks the user for who goes first
     also, it runs game_loop which starts the actual game 
     """
-
     def play(self):
+        print("Welcome to Connect the Four!\nPlayer 1 is X\nPlayer 2 is O\n")
         p1_first = self.turn(input("Who goes first? (p1/p2): "))
 
         self.game_loop(p1_first)
